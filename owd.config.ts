2266fb7d76401cf88806760cbe485e2eaaec841f
dxlliv <hello@dxlliv.dev>
2025-05-06 02:33:21 +0200

    Update to @owdproject/core alpha-0.0.8

--- owd.config.ts
+++ owd.config.ts
@@ -2,9 +2,16 @@ import {defineDesktopConfig} from "@owdproject/core/runtime/utils/utilsDesktop"
 
 export default defineDesktopConfig({
     apps: [
-        '@owdproject/app-about',
-        '@owdproject/app-debug',
-        '@owdproject/app-todo',
-        '@owdproject/app-wasmboy',
+        "@owdproject/app-todo",
+        "@owdproject/app-terminal",
+        "@owdproject/app-about",
+        "@owdproject/app-wasmboy",
+        "@owdproject/app-debug",
     ],
+
+    theme: "@owdproject/theme-gnome",
+
+    workspaces: {
+        enabled: false,
+    }
 })
